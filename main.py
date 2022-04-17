c820fa599985970f34f2b942efd6e3ef5c215bd2
Elaine <71743181+elainechan01@users.noreply.github.com>
2022-04-17 18:42:24 -0400

    update
    - add pop
    - clean main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # <----- READ ME ----->
-# Build a game using the concept of objects and classes. The main algorithm to implement to modify the burger is to use the concept of stacks and its theory of FIFO (stack implementation: https://www.tutorialspoint.com/data_structures_algorithms/stack_algorithm.htm). In other words, when adding ingredients to the burger, the ingredients will be added to the end of the list. The main implementation of the game can be found under the `main` function.
+# Build a game using the concept of objects and classes. The main algorithm to implement to modify the burger is to use the concept of stacks and its theory of LIFO (stack implementation: https://www.tutorialspoint.com/data_structures_algorithms/stack_algorithm.htm). In other words, when adding ingredients to the burger, the ingredients will be added to the end of the list. The main implementation of the game can be found under the `main` function.
 
 class Burger:
     def __init__(self):
@@ -7,29 +7,36 @@ class Burger:
 
     # <----- TO DO: finish this function that will take an ingredient as a string to be added to the burger ----->
     def add_ingredients(self, ingredient):
-        pass   
+        pass
 
     # <----- TO DO: finish this function to print the burger ----->
-    # Example burger: ["bun", "lettuce", "mayonnaise", "tomato", "patty", "cheese", "bun"]
+    # Example burger: ["patty", "cheese", "bun"]
     # Example output:
-    #  bun
-    #  cheese
-    #  patty
-    #  tomato
-    #  mayonnaise
-    #  lettuce
-    #  bun
+    # bun
+    # cheese
+    # patty
     def serve_burger(self):
+        pass
+    
+    # <----- TO DO: finish this function to remove the ingredient at the top of the stack ----->
+    # Example burger: ["patty", "cheese", "bun"]
+    # Element to be returned: bun
+    def pop_ingredient(self):
         pass
 
 def main():
     burger = Burger()
-    userInput = True
-    while userInput != "q":
-        userInput = input("Add your ingredient (q to quit): ")
-        burger.add_ingredients(userInput)
+    burger.add_ingredients('bun')
+    burger.add_ingredients('lettuce')
+    burger.add_ingredients('mayonnaise')
+    burger.add_ingredients('tomato')
+    burger.add_ingredients('patty')
+    burger.add_ingredients('cheese')
+    burger.add_ingredients('bun')
+    burger.add_ingredients('sesame seed')
+    topIngredient = burger.pop_ingredient()
+    print("Ingredient on top: " + topIngredient)    
     burger.serve_burger()
-    return 
 
 if __name__ == "__main__":
     main()
